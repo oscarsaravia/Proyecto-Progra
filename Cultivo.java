12968d13008c49c0f020551ecefc80796bc4d06c
Requetetin <martinamadog@gmail.com>
2019-08-29 12:01:32 -0600

    Modificacion y comienzo de cultivo y tierra

--- Cultivo.java
+++ Cultivo.java
@@ -2,8 +2,17 @@
 Recibe un objeto de tierra y a partir de los parametros genera un cultivo*/
 
 public class Cultivo{
+	private String cultivo;
+	private String departamento;
 
-	public generarCultivo(Tierra tierra){
+	public void generarCultivo(Tierra tierra){
+		int dept = tierra.getDepartamento();
+		int rain = tierra.getLluvia();
+		int clima = tierra.getClima();
 		
+		}
+
+	public String getCultivo(){
+		return cultivo;
 	}
 }
--- Tierra.java
+++ Tierra.java
@@ -2,25 +2,30 @@
 La clase, a partir de los parametros ingresados, genera un objeto tipo tierra*/
 
 public class Tierra{
-	private String region;
-	private double lluvia;
-	private String clima;
+	private int departamento;
+	private int lluvia;
+	private int clima;
+	private String[] departamentos = {"Alta Verapaz", "Baja Verapaz", "Chimaltenango", "Chiquimula", "El Progreso", "Escuintla","Guatemala","Huehuetenango","Izabal","Jalapa","Jutiapa","Peten","Quetzaltenango","Quiche", "Retalhuleu","Sacatepequez","San Marcos","Santa Rosa","Solola","Suchitepequez","Totonicapan","Zacapa"};
 
-	public void crearTierra(String region, double lluvia, String clima){
-		this.region = region;
+
+	public void crearTierra(int departamento, int lluvia, int clima){
+		this.departamento = departamento;
 		this.lluvia = lluvia;
 		this.clima = clima;
 	}
 
-	public String getRegion(){
-		return region;
+	
+
+	public String getDepartamento(){
+		String depart = departamentos[departamento-1];
+		return depart;
 	}
 
-	public double getLluvia(){
-		return lluvia;
+	public String getLluvia(){
+		return lluv;
 	}
 
 	public String getClima(){
-		return clima;
+		return climas;
 	}
 }
