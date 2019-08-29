76186f4a19ee0094860f2a7cc7ebf45df24834e5
Requetetin <martinamadog@gmail.com>
2019-08-29 11:06:45 -0600

    Se agrego la clase cultivo y los metodos de la clase tierra

---
+++ Cultivo.java
@@ -0,0 +1,9 @@
+/*Cultivo.java
+Recibe un objeto de tierra y a partir de los parametros genera un cultivo*/
+
+public class Cultivo{
+
+	public generarCultivo(Tierra tierra){
+		
+	}
+}
--- Tierra.java
+++ Tierra.java
@@ -1,6 +1,26 @@
 /*Tierra.java
-La clase, a partir de los parametros ingresados, genera el cultivo sugerido*/
+La clase, a partir de los parametros ingresados, genera un objeto tipo tierra*/
 
 public class Tierra{
-	private 
+	private String region;
+	private double lluvia;
+	private String clima;
+
+	public void crearTierra(String region, double lluvia, String clima){
+		this.region = region;
+		this.lluvia = lluvia;
+		this.clima = clima;
+	}
+
+	public String getRegion(){
+		return region;
+	}
+
+	public double getLluvia(){
+		return lluvia;
+	}
+
+	public String getClima(){
+		return clima;
+	}
 }
