ac9eb46ee39d3d2771bc1483a065a264b5afaa28
oscarsaravia <oscarsaraviard0013@gmail.com>
2019-09-03 12:09:14 -0600

    Interfaz gráfica (esta es)

---
+++ HelloFX/src/sample/Controller.java
@@ -0,0 +1,21 @@
+package sample;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Controller {
+    public void changeScreen(javafx.event.ActionEvent actionEvent)throws IOException {
+        Parent ventanaparent = FXMLLoader.load(getClass().getResource("C:/Users/oscar/IdeaProjects/HelloFX/src/sample/ventana2.fxml"));
+        Scene mitosView = new Scene(ventanaparent);
+
+        Stage window = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        window.setScene(mitosView);
+        window.show();
+
+    }
+}
