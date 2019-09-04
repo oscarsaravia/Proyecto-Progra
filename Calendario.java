a40a565349df38d54be98c882504b603bc1a2e2f
Olivverde <dele19270@uvg.edu.gt>
2019-09-04 11:36:47 -0600

    Se agrego la clase calendario

---
+++ Calendario.java
@@ -0,0 +1,93 @@
+//Oliver Josué de León Milian
+//Clase Calendario
+//Terra Plus
+//Almacena cada una de las calendarizaciones correspondiente a cada uno de los cultivos.
+
+public class Calendario {
+    //Métodos 
+
+    //Calendarización por cúltivo
+
+    //Cardamomo
+    //Frijol
+    //Caña
+    //Maíz
+    //Café
+    //Limón 
+    //Tomate
+    //Papa
+    //Chile
+    //Güisquil
+    //Güicoy
+    //Yuca
+    //Aguacate
+    //Rabano
+    //Remolacha
+    //Cebolla
+
+    //Información de fechas para Cardamomo
+    public void Cardamomo(){
+        
+    }
+     //Información de fechas para Frijol
+    public void Frijol(){
+        
+    }
+     //Información de fechas para Caña
+    public void Cana(){
+        
+    }
+     //Información de fechas para Maíz
+    public void Maiz(){
+        
+    }
+     //Información de fechas para Cafe
+    public void Cafe(){
+        
+    }
+     //Información de fechas para Limon
+    public void Limon(){
+        
+    }
+     //Información de fechas para Tomate
+    public void Tomate(){
+        
+    }
+     //Información de fechas para Papa
+    public void Papa(){
+        
+    }
+     //Información de fechas para Chile
+    public void Chile(){
+        
+    }
+     //Información de fechas para Güisquil
+    public void Guisquil(){
+        
+    }
+     //Información de fechas para Güicoy
+    public void Guicoy(){
+        
+    }
+     //Información de fechas para Yuca
+    public void Yuca(){
+        
+    }
+     //Información de fechas para Aguacate
+    public void Aguacate(){
+        
+    }
+     //Información de fechas para Rabano
+    public void Rabano(){
+        
+    }
+     //Información de fechas para Remolacha
+    public void Remolacha(){
+        
+    }
+     //Información de fechas para Cebolla
+    public void Cebolla(){
+        
+    }
+
+}
