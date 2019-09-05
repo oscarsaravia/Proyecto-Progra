89a35d48ba07a0e50293154cc679c85c8ad416a8
Oscar Andre <oaparedez17@gmail.com>
2019-09-04 22:08:18 -0600

    Se agrego color de fondo, botones faltantes y funcionalidad del boton de salir

--- HelloFX/src/sample/Controller.java
+++ HelloFX/src/sample/Controller.java
@@ -8,14 +8,23 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+
 public class Controller {
     public void changeScreen(javafx.event.ActionEvent actionEvent)throws IOException {
         Parent ventanaparent = FXMLLoader.load(getClass().getResource("C:/Users/oscar/IdeaProjects/HelloFX/src/sample/ventana2.fxml"));
         Scene mitosView = new Scene(ventanaparent);
 
         Stage window = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+
         window.setScene(mitosView);
         window.show();
 
+    }
+    public javafx.scene.control.Button closeButton;
+    public void closeButtonAction(){
+        // get a handle to the stage
+        Stage stage = (Stage) closeButton.getScene().getWindow();
+        // do what you have to do
+        stage.close();
     }
 }
--- HelloFX/src/sample/Main.java
+++ HelloFX/src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("PROYECTO");
+        primaryStage.setTitle("TERRA+");
         primaryStage.setScene(new Scene(root, 600, 275));
         primaryStage.show();
     }
