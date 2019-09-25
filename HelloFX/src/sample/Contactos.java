62c9830eddf26764d4652f555c5fe5d1ceb2e66e
oscarparedez <42584081+oscarparedez@users.noreply.github.com>
2019-09-25 13:11:42 -0600

    Add files via upload
    Se crearon todas las funcionalidades de las ventanas emergentes para todos los botones del menu

---
+++ HelloFX/src/sample/Contactos.java
@@ -0,0 +1,4 @@
+package sample;
+
+public class Contactos {
+}
--- HelloFX/src/sample/Controller.java
+++ HelloFX/src/sample/Controller.java
@@ -10,16 +10,62 @@ import java.io.IOException;
 
 
 public class Controller {
-    public void changeScreen(javafx.event.ActionEvent actionEvent)throws IOException {
-        Parent ventanaparent = FXMLLoader.load(getClass().getResource("C:/Users/oscar/IdeaProjects/HelloFX/src/sample/ventana2.fxml"));
-        Scene mitosView = new Scene(ventanaparent);
+
+    public void screenBiblioteca(javafx.event.ActionEvent actionEvent)throws IOException {
+        Parent screenBiblio = FXMLLoader.load(getClass().getResource("Cultivos.fxml"));
+        Scene biblio = new Scene(screenBiblio);
 
         Stage window = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
 
-        window.setScene(mitosView);
+        window.setScene(biblio);
         window.show();
 
     }
+
+    public void screenCultivos(javafx.event.ActionEvent actionEvent)throws IOException {
+        Parent screenCultiv = FXMLLoader.load(getClass().getResource("Cultivos.fxml"));
+        Scene cultiv = new Scene(screenCultiv);
+
+        Stage window = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+
+        window.setScene(cultiv);
+        window.show();
+
+    }
+
+    public void screenCalendario(javafx.event.ActionEvent actionEvent)throws IOException {
+        Parent screenCal = FXMLLoader.load(getClass().getResource("Calendario.fxml"));
+        Scene calen = new Scene(screenCal);
+
+        Stage window = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+
+        window.setScene(calen);
+        window.show();
+
+    }
+
+    public void screenTutoriales(javafx.event.ActionEvent actionEvent)throws IOException {
+        Parent screenT = FXMLLoader.load(getClass().getResource("Tutoriales.fxml"));
+        Scene tutor = new Scene(screenT);
+
+        Stage window = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+
+        window.setScene(tutor);
+        window.show();
+
+    }
+
+    public void screenContactos(javafx.event.ActionEvent actionEvent)throws IOException {
+        Parent screenC = FXMLLoader.load(getClass().getResource("Contactos.fxml"));
+        Scene contac = new Scene(screenC);
+
+        Stage window = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+
+        window.setScene(contac);
+        window.show();
+
+    }
+
     public javafx.scene.control.Button closeButton;
     public void closeButtonAction(){
         Stage stage = (Stage) closeButton.getScene().getWindow();
