dccd8538b02471dd4145361f4ff80dec0c7ca25d
Requetetin <martinamadog@gmail.com>
2019-09-25 20:43:56 -0600

    Modificacion catalogo

--- Catalogo.java
+++ Catalogo.java
@@ -3,12 +3,12 @@
 public class Catalogo{
 	private String[] catalogo = {"Cardamomo", "Frijol", "Cana", "Maiz", "Cafe", "Limon", "Tomate", "Papa", "Chile", "Guisquil", "Guicoy", "Yuca", "Aguacate", "Rabano", "Remolacha", "Cebolla"};
 
-	public String[] getCatalogo(){
+	public String getCatalogo(){
 		String lista = "Los cultivos disponibles son:\n";
 		for(int i=0; i<15; i++){
 			lista+= catalogo[i];
 			lista+= "\n";
 		}
-		return catalogo;
+		return lista;
 	}
 }
