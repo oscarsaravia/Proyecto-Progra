6e0ae126bd6293fafc799274c90b0bc64c5e8276
Requetetin <martinamadog@gmail.com>
2019-10-22 10:11:01 -0600

    Modificacion que permite que lo primero que se haga sea una encuesta para tener guardado un tipo de cultivo

--- Driver.java
+++ Driver.java
@@ -9,8 +9,32 @@ public class Driver{
 		Control con = new Control();
 		int opcion;
 
+		//Antes de entrar al ciclo, se hace la encuesta para siempre tener guardado un cultivo por lo menos
+		System.out.println("Bienvenido\nPara comenzar con el proceso se necesitara que conteste las siguientes preguntas");
+		System.out.println("En que clima esta ubicado?\n"); 
+		System.out.print("1. Calido: presenta, a lo largo de todo el año, temperaturas elevadas. Se trata de un clima habitual en las sabanas, las selvas y los bosques tropicales.\n"); 
+		System.out.print("2. Templado: caracterizado por temperaturas medias anuales que rondan los 15 grados centigrados\n"); 
+		System.out.print("3. Frio: se caracteriza por mantener temperaturas bajas a lo largo del ano\n");
+		int clima = scan.nextInt();
+		System.out.println("En que departamento se encuentra?");
+		con.getDept(clima);
+		System.out.println("");
+		scan.nextLine();
+		String departamento = scan.nextLine();
+		if(clima == 1){
+			con.recibirCultivo("Calido", departamento);
+			System.out.println(con.devolverCultivo("Calido", departamento)+"\n \n");
+		}
+		else if(clima == 2){
+			con.recibirCultivo("Templado", departamento);
+			System.out.println(con.devolverCultivo("Templado", departamento)+"\n \n");
+		}
+		else if(clima == 3){
+			con.recibirCultivo("Frio", departamento);
+			System.out.println(con.devolverCultivo("Frio", departamento)+"\n \n");
+		}
 		do{
-			System.out.println("Bienvenido\n Que desea hacer?\n 1. Conocer el cultivo que debe plantar\n 2. Calendario de su cultivo\n 3. Tutorial del cultivo\n 4. Salir\n 5. Listado de cultivos\n 6. Contactos");
+			System.out.println("\nQue desea hacer?\n1. Conocer el cultivo que debe plantar\n2. Calendario de su cultivo\n3. Tutorial del cultivo\n4. Listado de cultivos\n5. Contactos\n6. Salir\n");
 			opcion=scan.nextInt();
 			switch(opcion){
 				case 1:
@@ -18,12 +42,12 @@ public class Driver{
 				System.out.print("1. Calido: presenta, a lo largo de todo el año, temperaturas elevadas. Se trata de un clima habitual en las sabanas, las selvas y los bosques tropicales.\n"); 
 				System.out.print("2. Templado: caracterizado por temperaturas medias anuales que rondan los 15 grados centigrados\n"); 
 				System.out.print("3. Frio: se caracteriza por mantener temperaturas bajas a lo largo del ano\n");
-				int clima = scan.nextInt();
+				clima = scan.nextInt();
 				System.out.println("En que departamento se encuentra?");
 				con.getDept(clima);
 				System.out.println("");
 				scan.nextLine();
-				String departamento = scan.nextLine();
+				departamento = scan.nextLine();
 				if(clima == 1){
 					con.recibirCultivo("Calido", departamento);
 					System.out.println(con.devolverCultivo("Calido", departamento)+"\n \n");
@@ -46,22 +70,22 @@ public class Driver{
 				con.opcionTres();
 				break;
 
-				case 4:
-				System.out.println("\nGracias por utilizar los servicios, ha sido un gusto");
-				break;
-
-				case 5:
-				System.out.println(con.opcionCinco());
-				break;
-
-				/*case 6:
+				/*case 4:
 				System.out.println(con.opcionSeis());
 				int caso = scan.nextInt();
 				scan.nextLine();
 				System.out.println(con.seisParteDos(caso));
 				break;*/ //No implementado
+
+				case 5:
+				System.out.println(con.opcionCinco());
+				break;
+
+				case 6:
+				System.out.println("\nGracias por utilizar los servicios, ha sido un gusto");
+				break;
 			}
 		}
-		while(opcion!=4);
+		while(opcion!=6);
 	}
 }
