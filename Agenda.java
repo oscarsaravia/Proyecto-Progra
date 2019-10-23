0201036084e5f20cd8778a76c29946d57ea4feb6
oscarsaravia <oscarsaraviard0013@gmail.com>
2019-10-22 19:37:04 -0600

    Se arreglaron metodos de la clase agenda

--- Agenda.java
+++ Agenda.java
@@ -15,29 +15,34 @@ public class Agenda{
 	
 	Scanner scan = new Scanner(System.in);	
 	
-	Datos[0] = "Nombre: ";
-	Datos[1] = "Apellido: ";
-	Datos[2] = "Numero telefonico: ";
-	Datos[3] = "Correo electronico: ";
-	Datos[4] = "Empresa: ";
+	//METODO PARA AGREGAR LAS PROPIEDADES A LA AGENDA
+	public void iniciaragenda(){
+		Datos[0] = "Nombre: ";
+		Datos[1] = "Apellido: ";
+		Datos[2] = "Numero telefonico: ";
+		Datos[3] = "Correo electronico: ";
+		Datos[4] = "Empresa: ";
+	}
 		
 
 		
 	//USUARIOS POR DEFECTO
 		
-	agenda.add(new String[5]);
-	agenda.get(0)[0] = "Oscar";
-	agenda.get(0)[1] = "Saravia";
-	agenda.get(0)[2] = "55435678";
-	agenda.get(0)[3] = "oscarsaravia@hotmail.com";
-	agenda.get(0)[4] = "Terra";
-		
-	agenda.add(new String[5]);
-	agenda.get(1)[0] = "Carlos";
-	agenda.get(1)[1] = "Perez";
-	agenda.get(1)[2] = "54672309";
-	agenda.get(1)[3] = "carlitos@hotmail.com";
-	agenda.get(1)[4] = "Acma";
+	public void agregardatos(){
+		agenda.add(new String[5]);
+		agenda.get(0)[0] = "Oscar";
+		agenda.get(0)[1] = "Saravia";
+		agenda.get(0)[2] = "55435678";
+		agenda.get(0)[3] = "oscarsaravia@hotmail.com";
+		agenda.get(0)[4] = "Terra";
+			
+		agenda.add(new String[5]);
+		agenda.get(1)[0] = "Carlos";
+		agenda.get(1)[1] = "Perez";
+		agenda.get(1)[2] = "54672309";
+		agenda.get(1)[3] = "carlitos@hotmail.com";
+		agenda.get(1)[4] = "Acma";
+	}
 	
 	//METODO PARA DESPLEGAR LA INFORMACION DE LOS CONTACTOS GUARDADOS
 	public void DesplegarAgenda(){
